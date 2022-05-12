d5d248fd0dccbde3bfeee1d4153c4b19071dbff1
Nigel Martin <nigelmartinuk@outlook.com>
2022-05-13 00:11:09 +0100

    about page - displays backend_project 5 extra webpages added CSS & Javascript shared to all html via extending base.html Redirections add for /javascript /js /home 404 and Admin pages left TODO

--- backend_project/views.py
+++ backend_project/views.py
@@ -2,10 +2,14 @@ from flask import render_template, redirect, url_for, request, Blueprint
 
 alias = Blueprint('alias', __name__)
 
+# default route points to our colleague's original javascript page
 @alias.route("/")    
 def home():
     return render_template('index.html')
 
+# these three routes point to the default index.html page
+@alias.route("/javascript")
+@alias.route("/js")
 @alias.route("/home")
 def home_redirect():
     return redirect(url_for("alias.home"))
@@ -14,6 +18,22 @@ def home_redirect():
 def about():
     return render_template('about.html')    
 
-@alias.route("/contact")
-def contact():
-    return render_template('contact.html')
+@alias.route("/page1")
+def page1():
+    return render_template('page1.html')
+
+@alias.route("/page2")
+def page2():
+    return render_template('page2.html')
+
+@alias.route("/page3")
+def page3():
+    return render_template('page3.html')
+
+@alias.route("/page4")
+def page4():
+    return render_template('page4.html')
+
+@alias.route("/page5")
+def page5():
+    return render_template('page5.html')
